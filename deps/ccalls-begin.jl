7d95b699918d7c97312413495339ee6bcc49f2c4
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-02-15 23:57:06 +0100

    Use @__DIR__ macro

--- deps/ccalls-begin.jl
+++ deps/ccalls-begin.jl
@@ -4,8 +4,8 @@
 # Calls to C-API of Nüvü Camēras.
 #
 
-if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+if isfile(joinpath(@__DIR__,"..","deps","deps.jl"))
     include("../deps/deps.jl")
 else
-    error("NuvuCameras not properly installed.  Please run `Pkg.build(\"NuvuCameras\")` to create file \"",joinpath(dirname(@__FILE__),"..","deps","deps.jl"),"\"")
+    error("NuvuCameras not properly installed.  Please run `Pkg.build(\"NuvuCameras\")` to create file \"",joinpath(@__DIR__,"..","deps","deps.jl"),"\"")
 end
